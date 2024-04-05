f36de6bd28901b6679e1336e926317ca1b84b7b1
Santy2304 <a20210751@pucp.edu.pe>
2024-04-04 20:57:39 -0500

    correción y creación de html en develop

---
+++ src/main/java/com/example/lab1/Controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.example.lab1.Controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/principal",method =RequestMethod.GET)
+
+    public String paginaPrincipal(){
+        return "principal";
+    }
+
+}
